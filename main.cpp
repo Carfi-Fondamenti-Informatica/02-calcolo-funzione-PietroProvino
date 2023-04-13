8e8459e737dc35cf1b92665b727e83167c308db9
PietroProvino <127682352+PietroProvino@users.noreply.github.com>
2023-04-13 13:42:36 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,20 @@
 using namespace std;
 
 int main() {
-   return 0;
-}
+    float a, b, x, y, z;
+    cout << "inserisci tre valori reali:\n";
+    cin >> a >> b >> x >> y;
+    if ((x<0)&&(y>0)){
+        z=(a*x)-(b*y);
+        cout << z;
+        }
+    else if ((x>=0)&&(y<=0)){
+        z=(a*(x*x))-(b*y);
+        cout << z ;
+        }
+    else{
+        z=(a*x)-(b*(y*y));
+        cout << z ;
+        }
+    return 0;
+    }
